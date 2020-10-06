6f628913f835bbb372ff70be6bb5f4a3b0a99e71
Hasitha Kaushan <hkraclh@gmail.com>
2020-10-06 22:23:37 +0530

    Google signin added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Auth } from 'aws-amplify';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'to-do-app';
+
+  googleSignIn(){
+    Auth.federatedSignIn({customProvider: 'Facebook'})
+  }
+
 }
